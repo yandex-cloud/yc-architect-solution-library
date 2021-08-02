dcd38412a96b499681bbac77fff4b9aa4fed0737
mirtov-alexey <sobolevalexwee@yandex.ru>
2021-08-02 14:09:24 +0300

    add ELK SIEM solution - mirtov

---
+++ security-solution-library/auditlogs/export-auditlogs-to-ELK/README.md
@@ -0,0 +1,162 @@
+# Сбор, мониторинг и анализ аудит логов в Yandex Managed Service for Elasticsearch (ELK)
+
+![dashboard](https://user-images.githubusercontent.com/85429798/127686785-27658104-6258-4de8-929f-9cf87624fa27.png)
+
+## Оглавление
+- [Сбор, мониторинг и анализ аудит логов в Yandex Managed Service for Elasticsearch (ELK)](#----------------------------------------yandex-managed-service-for-elasticsearch--elk-)
+  * [Оглавление](#----------)
+  * [Описание решения](#-----------------)
+  * [Что делает решение](#-------------------)
+  * [Схема решения](#-------------)
+  * [Security Content](#security-content)
+  * [Лицензионные ограничения](#------------------------)
+  * [Процесс обновления контента](#---------------------------)
+  * [Развертывание с помощью Terraform](#------------------------terraform)
+      - [Описание](#--------)
+      - [Пререквизиты](#------------)
+      - [Пример вызова модулей:](#----------------------)
+  * [Аудит логи Falco](#-----------falco)
+  * [Аудит логи k8s](#-----------k8s)
+
+<small><i><a href='http://ecotrust-canada.github.io/markdown-toc/'>Table of contents generated with markdown-toc</a></i></small>
+
+
+## Описание решения
+Решение позволяет собирать, мониторить и анализировать аудит логи в Yandex.Cloud со следующих источников:
+- [Yandex Audit Trails](https://cloud.yandex.ru/docs/audit-trails/)
+- [Falco](https://falco.org/) ([действия для настройки Falco см.](https://github.com/yandex-cloud/yc-solution-library-for-security/tree/master/auditlogs/export-auditlogs-to-ELK#аудит-логи-falco) )
+- Скоро: [Yandex Managed Service for Kubernetes](https://cloud.yandex.ru/services/managed-kubernetes)
+
+Решение является постоянно обновляемым и поддерживаемым Security командой Yandex.Cloud
+
+## Что делает решение
+- [x] Разворачивает в инфраструктуре Yandex.Cloud cluster Managed ELK (возможно через Terraform)
+- [x] Разворачивает COI Instance с контейнером на базе образа s3-elk-importer (cr.yandex/crpjfmfou6gflobbfvfv/s3-elk-importer:latest)
+- [x] Загружает Security Content в ELK (Dashboards, Detection Rules (с alerts), etc.)
+- [x] Обеспечивает непрерывную доставку json файлов с аудит логами из Yandex Object Storage в ELK
+
+## Схема решения
+![Схема_решения](https://user-images.githubusercontent.com/85429798/127733565-d455b897-d1ca-4d43-8ee9-195bb7a7d5ab.png)
+
+
+## Security Content
+Security Content - объекты ELK, которые автоматически загружаются решением. Весь контент разработан с учетом многолетнего опыта Security команды Yandex.Cloud и на основе опыта Клиентов облака.
+
+Содержит следующий Security Content:
+- Dashboard, на котором отражены все use cases и полезная статистика
+- Набор Saved Queries для удобного поиска Security событий
+- Набор Detection Rules (правила корреляции) на которые настроены оповещения (Клиенту самостоятельно необходимо указать назначение уведомлений)
+- Все интересные поля событий преобразованы в формат [Elastic Common Schema (ECS)](https://www.elastic.co/guide/en/ecs/current/index.html), полная табличка маппинга в файле [/papers/Описание объектов.pdf](ссылка)
+
+Подробное описание в файле [/papers/ECS-mapping.docx](https://github.com/yandex-cloud/yc-solution-library-for-security/blob/master/auditlogs/export-auditlogs-to-ELK/papers/ECS-mapping_new.pdf)
+
+
+## Лицензионные ограничения
+
+![image](https://user-images.githubusercontent.com/85429798/127733756-1a751356-a0f3-492e-9a85-56d3a73e298f.png)
+![image](https://user-images.githubusercontent.com/85429798/127733769-5ee2f70a-2162-487f-b236-9076c6d9c681.png)
+[Описание различий с сайта ELK](https://www.elastic.co/subscriptions)
+
+## Процесс обновления контента
+Рекомендуется подписаться на обновления в данном репозитории для оповещений о наличии нового обновления.
+Технически обновление контента выполняется за счет обновления версии контейнера на новую версию cr.yandex/crpjfmfou6gflobbfvfv/s3-elk-importer:latest
+
+Для обновления необходимо обеспечить удаления текущией версии image, скачивание новой версии, пересоздание контейнера:
+- либо пересоздать COI Instance через terraform (который был использован для его деплоя)
+- либо выполнить вышеуказанные действия вручную (удалить imnage, удалить контейнер, перезагрузить ВМ)
+
+
+## Развертывание с помощью Terraform
+
+#### Описание 
+
+#### Пререквизиты
+- :white_check_mark: Object Storage Bucket для AuditTrails
+- :white_check_mark: Включенный сервис AuditTrail в UI
+- :white_check_mark: Сеть VPC
+- :white_check_mark: Подсети в 3-х зонах доступности
+- :white_check_mark: Наличие доступа в интернет с COI Instance для скачивания образа контейнера
+- :white_check_mark: ServiceAccount с ролью storage.editor для действий в Object Storage
+
+См. Пример конфигурации пререквизитов в [/example/main.tf](https://github.com/yandex-cloud/yc-solution-library-for-security/tree/master/auditlogs/export-auditlogs-to-ELK/terraform/example) 
+Решение состоит из 2-х модулей Terraform [/terraform/modules/](https://github.com/yandex-cloud/yc-solution-library-for-security/tree/master/auditlogs/export-auditlogs-to-ELK/terraform/modules) :
+1) yc-managed-elk:
+- создает cluster [Yandex Managed Service for Elasticsearch](https://cloud.yandex.ru/services/managed-elasticsearch) 
+- с 3 нодами (1 на зону доступности) 
+- с лицензией Gold
+- характеристики: s2-medium (8vCPU, 32Gb Memory)
+- HDD: 1TB
+- назначает пароль на аккаунт admin в ELK
+
+2) yc-elastic-trail:
+- создает static keys для sa (для работы с объектами JSON в бакете и шифрования/расшифрования секретов)
+- создает ВМ COI со спецификацией Docker Container со скриптом
+- создает ssh пару ключей и сохраняет приватную часть на диск, публичную в ВМ
+- создает KMS ключ
+- назначает права kms.keys.encrypterDecrypter на ключ для sa для шифрование секретов
+- шифрует секреты и передает их в Docker Container
+
+
+#### Пример вызова модулей:
+```Python
+module "yc-managed-elk" {
+    source = "../modules/yc-managed-elk" #path to module yc-managed-elk
+    
+    folder_id = var.folder_id
+    subnet_ids = ["e9boih92qspkol5morvl", "e2lbe671uvs0i8u3cr3s", "b0c0ddsip8vkulcqh7k4"]  #subnets в 3-х зонах доступности для развертывания ELK
+    network_id = "enp5t00135hd1mut1to9" # network id в которой будет развернут ELK
+}
+
+
+
+module "yc-elastic-trail" {
+    source = "../modules/yc-elastic-trail/" #path to module yc-elastic-trail
+    
+    folder_id = var.folder_id
+    cloud_id = var.cloud_id
+    elk_credentials = module.yc-managed-elk.elk-pass
+    elk_address = module.yc-managed-elk.elk_fqdn
+    bucket_name = "bucket-mirtov8"
+    bucket_folder = "folder"
+    sa_id = "aje5h5587p1bffca503j"
+    coi_subnet_id = "e9boih92qspkol5morvl"
+}
+
+output "elk-pass" {
+      value = module.yc-managed-elk.elk-pass
+      sensitive = true
+    }
+//Чтобы посмотреть пароль ELK: terraform output elk-pass
+
+output "elk_fqdn" {
+      value = module.yc-managed-elk.elk_fqdn
+    }
+    
+//Выводит адрес ELK на который можно обращаться, например через браузер 
+
+output "elk-user" {
+      value = "admin"
+    }
+    
+```
+
+## Аудит логи Falco
+Экспорт событий Falco выполняется с помощью [Falco Sidekick](https://github.com/falcosecurity/falcosidekick)
+
+Для установки Falco и Falco Sidekick используйте следующие команды (замените значения falcosidekick.config.elasticsearch.hostport, username, password на ваши):
+
+```Python
+helm repo add falcosecurity https://falcosecurity.github.io/charts
+helm repo update
+
+helm install falco falcosecurity/falco \
+--set falcosidekick.enabled=true \
+--set falcosidekick.config.elasticsearch.hostport=https://c-c9qps9eabd0ok4haehjq.rw.mdb.yandexcloud.net:9200 \
+--set falcosidekick.config.elasticsearch.username=beat \
+--set falcosidekick.config.elasticsearch.password=beat1234 \
+--set falcosidekick.config.elasticsearch.checkcert=false \
+--set falcosidekick.config.debug=true
+```
+
+## Аудит логи k8s
+Скоро
---
+++ security-solution-library/auditlogs/export-auditlogs-to-ELK/functions/main.py
@@ -0,0 +1,238 @@
+import requests
+import json
+import os
+import boto3
+import time
+import base64
+
+# Function - Get token
+def get_token():
+    response = requests.get('http://169.254.169.254/computeMetadata/v1/instance/service-accounts/default/token', headers={"Metadata-Flavor":"Google"})
+    return response.json().get('access_token')
+
+# Function - Decrypt data with KMS key
+def decrypt_secret_kms(secret):
+    token = get_token()
+    request_suffix = kms_key_id+':decrypt'
+    request_json_data = {'ciphertext': secret}
+    response = requests.post('https://kms.yandex/kms/v1/keys/'+request_suffix, data=json.dumps(request_json_data), headers={"Accept":"application/json", "Authorization": "Bearer "+token})
+    b64_data = response.json().get('plaintext')
+    return base64.b64decode(b64_data).decode()
+
+# Configuration - Keys
+kms_key_id              = os.environ['KMS_KEY_ID']
+elastic_auth_pw_encr    = os.environ['ELK_PASS_ENCR']
+s3_key_encr             = os.environ['S3_KEY_ENCR']
+s3_secret_encr          = os.environ['S3_SECRET_ENCR']
+
+# Configuration - Setting up variables for ElasticSearch
+elastic_server      = os.environ['ELASTIC_SERVER']
+elastic_auth_user   = os.environ['ELASTIC_AUTH_USER']
+elastic_auth_pw     = decrypt_secret_kms(elastic_auth_pw_encr)
+elastic_index_name  = os.environ['ELASTIC_INDEX_NAME']
+kibana_server       = os.environ['KIBANA_SERVER']
+elastic_cert        = 'include/ca.pem'
+
+# Configuration - Setting up variables for S3
+s3_key              = decrypt_secret_kms(s3_key_encr)
+s3_secret           = decrypt_secret_kms(s3_secret_encr)
+s3_bucket           = os.environ['S3_BUCKET']
+s3_folder           = os.environ['S3_FOLDER']
+s3_local            = '/tmp/s3'
+
+# Configuration - Sleep time
+if(os.getenv('SLEEP_TIME') is not None):
+    sleep_time = int(os.environ['SLEEP_TIME'])
+else:
+    sleep_time = 240
+
+# State - Setting up S3 client
+s3 = boto3.resource('s3',
+    endpoint_url='https://storage.yandexcloud.net',
+    aws_access_key_id = s3_key,
+    aws_secret_access_key = s3_secret 
+)
+
+# Function - Create config index in ElasticSearch
+def create_config_index():
+    request_suffix = '/.state-'+elastic_index_name
+    response = requests.get(elastic_server+request_suffix, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw))
+    if(response.status_code == 404):
+        request_suffix = '/.state-'+elastic_index_name+'/_doc/1'
+        request_json = """{
+            "is_configured": true
+        }"""
+        response = requests.post(elastic_server+request_suffix, data=request_json, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw), headers={"Content-Type":"application/json"})
+        print('Config index -- CREATED')
+    else:
+        print('Config index -- EXISTS')
+
+# Function - Get config index state
+def get_config_index_state():
+    request_suffix = '/.state-'+elastic_index_name+'/_doc/1/_source'
+    response = requests.get(elastic_server+request_suffix, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw))
+    if(response.status_code != 200):
+        return False
+    return response.json()['is_configured']
+
+# Function - Create ingest pipeline
+def create_ingest_pipeline():
+    request_suffix = '/_ingest/pipeline/audit-trails-pipeline'
+    data_file = open('include/elasticsearch/pipeline.json')
+    data_json = json.load(data_file)
+    data_file.close()
+    response = requests.put(elastic_server+request_suffix, json=data_json, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw))
+    if(response.status_code == 200):
+        print('Ingest pipeline -- CREATED')
+
+# Function - Create an index with mapping
+def create_index_with_map():
+    request_suffix = '/audit-trails-index'
+    data_file = open('include/elasticsearch/mapping.json')
+    data_json = json.load(data_file)
+    data_file.close()
+    response = requests.put(elastic_server+request_suffix, json=data_json, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw))
+    if(response.status_code == 200):
+        print('Index with mapping -- CREATED')
+
+# Function - Refresh index
+def refresh_index():
+    request_suffix = '/'+elastic_index_name+'/_refresh'
+    response = requests.post(elastic_server+request_suffix, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw))
+    print('Index -- REFRESHED')
+
+# Function - Preconfigure Kibana
+def configure_kibana():
+    # Index pattern
+    data_file = {
+        'file': open('include/kibana/index_pattern.ndjson', 'rb')
+    }
+    request_suffix = '/api/saved_objects/_import'
+    response = requests.post(kibana_server+request_suffix, files=data_file, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw), headers={"kbn-xsrf":"true"})
+    if(response.status_code == 200):
+        print('Index patterns -- IMPORTED')
+
+    # Filters
+    data_file = {
+        'file': open('include/kibana/filters.ndjson', 'rb')
+    }
+    request_suffix = '/api/saved_objects/_import'
+    response = requests.post(kibana_server+request_suffix, files=data_file, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw), headers={"kbn-xsrf":"true"})
+    if(response.status_code == 200):
+        print('Filters -- IMPORTED')
+
+    # Search
+    data_file = {
+        'file': open('include/kibana/search.ndjson', 'rb')
+    }
+    request_suffix = '/api/saved_objects/_import'
+    response = requests.post(kibana_server+request_suffix, files=data_file, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw), headers={"kbn-xsrf":"true"})
+    if(response.status_code == 200):
+        print('Searches -- IMPORTED')
+
+    # Dashboard
+    data_file = {
+        'file': open('include/kibana/dashboard.ndjson', 'rb')
+    }
+    request_suffix = '/api/saved_objects/_import'
+    response = requests.post(kibana_server+request_suffix, files=data_file, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw), headers={"kbn-xsrf":"true"})
+    if(response.status_code == 200):
+        print('Dashboard -- IMPORTED')
+
+    # Detections (not stable, throws error 400 from time to time)
+    data_file = {
+        'file': open('include/kibana/detections.ndjson', 'rb')
+    }
+    request_suffix = '/api/detection_engine/rules/_import'
+    response = requests.post(kibana_server+request_suffix, files=data_file, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw), headers={"kbn-xsrf":"true"})
+    if(response.status_code == 200):
+        print('Detections -- IMPORTED')
+
+# Function - Download JSON logs to local folder
+def download_s3_folder(s3_bucket, s3_folder, local_folder=None):
+    print('JSON download -- STARTED')
+    bucket = s3.Bucket(s3_bucket)
+    if not os.path.exists(local_folder):
+            os.makedirs(local_folder)
+    for obj in bucket.objects.filter(Prefix=s3_folder):
+        target = obj.key if local_folder is None \
+            else os.path.join(local_folder, os.path.relpath(obj.key, s3_folder))
+        if not os.path.exists(local_folder):
+            os.makedirs(local_folder)
+        if obj.key[-1] == '/':
+            continue
+        # Downloading JSON logs in a flat-structured way
+        bucket.download_file(obj.key, local_folder+'/'+target.rsplit('/')[-1])
+    print('JSON download -- COMPLETE')
+
+# Function - Clean up S3 folder
+def delete_objects_s3(s3_bucket, s3_folder):
+    bucket = s3.Bucket(s3_bucket)
+    for obj in bucket.objects.filter(Prefix=s3_folder):
+        if(obj.key != s3_folder+'/'):
+            bucket.delete_objects(
+                Delete={
+                    'Objects': [
+                        {
+                            'Key': obj.key
+                        },
+                    ]
+                }
+            )
+    print('S3 bucket -- EMPTIED')
+
+# Function - Upload logs to ElasticSearch
+def upload_docs_bulk(s3_bucket, s3_folder):
+    print('JSON upload -- STARTED')
+    request_suffix = '/'+elastic_index_name+'/_bulk?pipeline=audit-trails-pipeline'
+    error_count = 0
+
+    for f in os.listdir(s3_local):
+        if f.endswith(".json"):
+            with open(s3_local+"/"+f, "r") as read_file:
+                data = json.load(read_file)
+            result = [json.dumps(record) for record in data]
+            with open(s3_local+'/nd-temp.json', 'w') as obj:
+                for i in result:
+                    obj.write('{"index":{}}\n')
+                    obj.write(i+'\n')
+            
+            data_file = open(s3_local+'/nd-temp.json', 'rb').read()
+            response = requests.post(elastic_server+request_suffix, data=data_file, verify=elastic_cert, auth=(elastic_auth_user, elastic_auth_pw), headers={"Content-Type":"application/x-ndjson"})
+            os.remove(s3_local+"/"+f)
+            if(response.status_code != 200):
+                error_count += 1
+                print(response.text)
+    if(os.path.exists(s3_local+'/nd-temp.json')):
+        os.remove(s3_local+'/nd-temp.json')
+    print('JSON upload -- COMPLETE -- %s ERRORS' % error_count)
+    if(error_count == 0):
+        delete_objects_s3(s3_bucket, s3_folder)
+    refresh_index()
+
+# Process - Upload data
+def upload_logs():
+    if(get_config_index_state()):
+        print("Config index -- EXISTS")
+        download_s3_folder(s3_bucket, s3_folder, s3_local)
+        upload_docs_bulk(s3_bucket, s3_folder)
+    else:
+        create_index_with_map()
+        create_ingest_pipeline()
+        configure_kibana()
+        create_config_index()
+        download_s3_folder(s3_bucket, s3_folder, s3_local)
+        upload_docs_bulk(s3_bucket, s3_folder)
+
+### MAIN CONTROL PANEL
+
+upload_logs()
+# get_config_index_state()
+# update_config_index_state()
+# get_config_index_state()
+# upload_docs_bulk()
+# download_s3_folder(s3_bucket, s3_folder, s3_local)
+# upload_docs_bulk()
+# refresh_index()
+print("Sleep -- STARTED")
+time.sleep(sleep_time)
