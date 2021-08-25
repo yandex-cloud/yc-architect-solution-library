b5d84af9f8bb7705c8bd872c2154d46ba796cb96
khlupnov <m.khlupnov@gmail.com>
2021-08-25 07:53:45 +0300

    SpeechKit adoption pack - Streaming batch audiofile ASR client

---
+++ yc-ai/yc-speechkit-batch-asr/SkTaskModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkBatchAsrClient
+{
+    public class SkTaskModel
+    {
+        public string Path { get; set; }
+        public string AudioUrl { get; set; }       
+        public string TaskId { get; set; }
+    }
+}
