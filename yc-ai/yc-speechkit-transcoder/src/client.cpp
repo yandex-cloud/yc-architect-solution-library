21383d98855ae2e338ea66a734b7b8db6a85fb3f
khlupnov <m.khlupnov@gmail.com>
2021-09-18 11:36:36 +0300

    grpc make

---
+++ yc-ai/yc-speechkit-transcoder/src/client.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char** argv)
+{
+
+	if (argc != 4) {
+		std::cout << "Usage asr-client -config <path_to_cfg_file> -asr-source <path_or_url_to_asr>" << std::endl;
+		return -1;
+	}
+
+}
