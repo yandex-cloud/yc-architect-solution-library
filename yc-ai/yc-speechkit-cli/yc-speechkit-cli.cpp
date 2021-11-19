bc4fbb8de18bf090852fb3ce6de0c801fb859326
khlupnov <m.khlupnov@gmail.com>
2021-11-19 11:25:22 +0300

    submodule for

---
+++ yc-ai/yc-speechkit-cli/yc-speechkit-cli.cpp
@@ -0,0 +1,12 @@
+﻿// yc-speechkit-cli.cpp : Defines the entry point for the application.
+//
+
+#include "yc-speechkit-cli.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
