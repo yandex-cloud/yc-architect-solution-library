e2b1cab4ac07429966edce0078725ea1da09cba8
Maxim Khlupnov <m.khlupnov@inbox.ru>
2022-05-29 11:56:36 +0300

    Controls for Hints

--- yc-ai/yc-ai-web-ui/wwwroot/js/site.js
+++ yc-ai/yc-ai-web-ui/wwwroot/js/site.js
@@ -2,3 +2,7 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+    $( function() {
+        $("#accordion").accordion();
+  } );
+
