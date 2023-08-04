edc7d303d14550dd19d14d10b11837d82500ec88
Alexander Dushein <adushein@yandex-team.ru>
2023-08-04 17:33:31 +0300

    Add options for select between image-family and image-id

--- yc-gitlab-runner-docker-machine/network.tf
+++ yc-gitlab-runner-docker-machine/network.tf
@@ -1,24 +1,24 @@
 module "network" {
-  count = var.subnet_id == null ? 1 : 0
-  source = "./modules/network"
-  cloud_id = local.cloud_id
-  folder_id = local.folder_id
-  network_id = var.network_id
-  network_create = var.network_id == null
-  network_name = var.network_name
+  count               = var.subnet_id == null ? 1 : 0
+  source              = "./modules/network"
+  cloud_id            = local.cloud_id
+  folder_id           = local.folder_id
+  network_id          = var.network_id
+  network_create      = var.network_id == null
+  network_name        = var.network_name
   network_description = var.network_description
   subnets = [
     {
-      "purpose": var.purpose,
+      "purpose" : var.purpose,
       "zone" : var.default_zone,
       "v4_cidr_blocks" : var.network_cidr,
-      "route_table": "inet-access"
+      "route_table" : "inet-access"
     }
   ]
   route_tables = [
     {
-      "name": "inet-access",
-      "routes": [
+      "name" : "inet-access",
+      "routes" : [
         {
           "destination_prefix" : "0.0.0.0/0",
           "next_hop_address" : "gateway"
--- yc-gitlab-runner-docker-machine/variables.tf
+++ yc-gitlab-runner-docker-machine/variables.tf
@@ -1,155 +1,161 @@
 variable "cloud_id" {
-  type = string
+  type        = string
   description = "cloud-id"
-  default = null
+  default     = null
 }
 
 variable "folder_id" {
-  type = string
+  type        = string
   description = "folder-id"
-  default = null
+  default     = null
 }
 
 variable "network_create" {
-  type = bool
+  type        = bool
   description = "create the network?"
-  default = true
+  default     = true
 }
 
 variable "security_group_create" {
-  type = bool
+  type        = bool
   description = "create security group(s)?"
-  default = true
+  default     = true
 }
 
 variable "network_id" {
-  type = string
+  type        = string
   description = "Existing network_id(vpc-id) where resources will be created"
-  default = null
+  default     = null
 }
 
 variable "network_name" {
-  type = string
+  type        = string
   description = "Network name"
-  default = "docker-machine"
+  default     = "docker-machine"
 }
 
 variable "network_description" {
-  type = string
+  type        = string
   description = "Network description"
-  default = "autocreated docker-machine network"
+  default     = "autocreated docker-machine network"
 }
 
 variable "network_cidr" {
-  type = string
+  type        = string
   description = "network cidr"
-  default = "10.11.12.0/24"
+  default     = "10.11.12.0/24"
 }
 
 variable "subnet_id" {
-  type = string
+  type        = string
   description = "Existing subnet id"
-  default = null
+  default     = null
 }
 
 variable "default_zone" {
-  type = string
+  type        = string
   description = "Default availability zone"
-  default = "ru-central1-a"
+  default     = "ru-central1-a"
 }
 
 variable "default_region" {
-  type = string
+  type        = string
   description = "Default Yandex Cloud region"
-  default = "ru-central1"
+  default     = "ru-central1"
 }
 
 variable "purpose" {
-  type = string
+  type        = string
   description = "Virtual machine purpose (prod, dev, stage, etc)"
-  default = "docker-machine"
+  default     = "docker-machine"
 }
 
 variable "username" {
-  type = string
+  type        = string
   description = "Initialzation username"
-  default = "ubuntu"
+  default     = "ubuntu"
 }
 
 variable "user_pubkey_filename" {
   description = "ssh public key filename"
-  type = string
-  default = "~/.ssh/id_rsa.pub"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
 }
 
 variable "gitlab_url" {
-  type = string
+  type        = string
   description = "gitlab url"
 }
 
 variable "gitlab_registration_token" {
-  type = string
+  type        = string
   description = "gitlab registration token"
 }
 
 variable "gitlab_runner_tags" {
-  type = string
+  type        = string
   description = "gitlab runner tags"
-  default = ""
+  default     = ""
 }
 
 variable "worker_runners_limit" {
-  type = string
+  type        = string
   description = "Maximum number of parallel workers"
-  default = "10"
+  default     = "10"
 }
 
 variable "worker_use_internal_ip" {
-  type = bool
+  type        = bool
   description = "yandex-use-internal-ip"
-  default = true
+  default     = true
 }
 
 variable "worker_image_family" {
-  type = string
+  type        = string
   description = "yandex-image-family"
-  default = "ubuntu-2204-lts"
+  default     = "ubuntu-2204-lts"
+}
+
+variable "worker_image_id" {
+  type        = string
+  description = "yandex-image-id"
+  default     = null
 }
 
 variable "worker_cores" {
-  type = string
+  type        = string
   description = "yandex-cores"
-  default = "4"
+  default     = "4"
 }
 
 variable "worker_disk_type" {
-  type = string
+  type        = string
   description = "yandex-disk-type"
-  default = "network-ssd-nonreplicated"
+  default     = "network-ssd-nonreplicated"
 }
 
 variable "worker_disk_size" {
-  type = string
+  type        = string
   description = "yandex-disk-size"
-  default = "93"
+  default     = "93"
 }
 
 variable "worker_memory" {
-  type = string
+  type        = string
   description = "yandex-memory"
-  default = "8"
+  default     = "8"
 }
 
 variable "worker_preemptible" {
-  type = bool
+  type        = bool
   description = "yandex-preemptible"
-  default = true
+  default     = true
 }
 
 variable "worker_platform_id" {
-  type = string
+  type        = string
   description = "yandex-platform-id"
-  default = "standard-v3"
+  default     = "standard-v3"
 }
 
 
