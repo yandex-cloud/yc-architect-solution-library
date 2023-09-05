6bbd848e9983be7d64cdb4d43d3adac70dae707b
Alexander Dushein <adushein@yandex-team.ru>
2023-09-05 08:35:12 +0300

    Remove import url for gitlab project

--- demos/failure-testing/bootstrap/gitlab.tf
+++ demos/failure-testing/bootstrap/gitlab.tf
@@ -17,7 +17,6 @@ resource "gitlab_project" "this" {
   name                = "failure-testing"
   description         = "Failure testing project"
   namespace_id        = data.gitlab_user.user.namespace_id
-  import_url          = "${var.gitlab_url}/gitops/chaos.git"
   import_url_username = var.gitlab_username
   import_url_password = var.gitlab_access_token
   lifecycle {
